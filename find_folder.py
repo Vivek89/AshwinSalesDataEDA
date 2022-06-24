a3467fe74002bd0f8cfca04bd7fc01833bb61479
Vivek Mishra <Vivek89@users.noreply.github.com>
2022-06-24 17:55:08 +0530

    udpate

--- find_folder.py
+++ find_folder.py
@@ -2,10 +2,30 @@ import os
 import shutil
 from distutils.dir_util import copy_tree
 
-_FOLDERS = '456;Option'
+import logging
+import sys
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.DEBUG)
+stdout_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler('logs.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+
+logger.addHandler(file_handler)
+logger.addHandler(stdout_handler)
+
+
+_FOLDERS = '123;Option'
 walk_dir = r'books'
 
-_des_dir = r'books/123/'
+_des_dir = r'books/456/'
 
 print('walk_dir = ' + walk_dir)
 
@@ -16,7 +36,7 @@ for root, subdirs, files in os.walk(walk_dir):
     print('--\nroot = ' + root)
     folder_name = root[root.rfind('/')+1:]
     if folder_name in _FOLDERS.split(";"):
-        print( "Found: ", folder_name)
+        logger.info( "Found: %s", folder_name)
         copy_tree(root, _des_dir) 
 
     # list_file_path = os.path.join(root, 'my-directory-list.txt')
@@ -29,4 +49,4 @@ for root, subdirs, files in os.walk(walk_dir):
     for filename in files:
         file_path = os.path.join(root, filename)
 
-        print('\t- file %s (full path: %s)' % (filename, file_path))
+        logger.info('\t- file %s (full path: %s)' % (filename, file_path))
